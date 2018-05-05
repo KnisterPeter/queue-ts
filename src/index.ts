9e2d8409f1b69b3ed34e96607dad55d0ad7f5106
Markus Wolf <mail@markus-wolf.de>
2018-05-05 12:31:27 +0200

    refactor: update linter configuration

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,20 @@
+export type Fn = (...args: any[]) => any;
+
 export class Queue {
 
-  private concurrency: number;
+  private readonly concurrency: number;
 
-  private workers: Promise<any>[] = [];
+  private readonly workers: Promise<any>[] = [];
 
-  private tasks: Function[] = [];
+  private readonly tasks: Fn[] = [];
 
-  private emptyCallback: Function;
+  private emptyCallback: Fn;
 
   constructor(concurrency: number) {
     this.concurrency = concurrency;
     for (let i = 0; i < concurrency; i++) {
       this.workers[i] = Promise.resolve();
     }
-  }
-
-  public add(task: Function): void {
-    if (this.isAvailable()) {
-      this.schedule(task);
-    } else {
-      this.tasks.push(task);
-    }
-  }
-
-  private schedule(task: Function): void {
-    const worker = this.workers.pop();
-    worker.then(() => {
-      return task();
-    }).then(() => {
-      this.workers.push(worker);
-      if (this.tasks.length > 0) {
-        this.schedule(this.tasks.pop());
-      } else if (this.isEmpty() && this.emptyCallback) {
-        this.emptyCallback();
-      }
-    });
   }
 
   public isAvailable(): boolean {
@@ -45,8 +25,31 @@ export class Queue {
     return this.tasks.length === 0 && this.workers.length === this.concurrency;
   }
 
-  public onEmpty(emptyCallback: Function): void {
+  public onEmpty(emptyCallback: Fn): void {
     this.emptyCallback = emptyCallback;
+  }
+
+  public add(task: Fn): void {
+    if (this.isAvailable()) {
+      this.schedule(task);
+    } else {
+      this.tasks.push(task);
+    }
+  }
+
+  private schedule(task: Fn): void {
+    const worker = this.workers.pop();
+    // tslint:disable-next-line:no-floating-promises
+    worker.then(() => {
+      return task();
+    }).then(() => {
+      this.workers.push(worker);
+      if (this.tasks.length > 0) {
+        this.schedule(this.tasks.pop());
+      } else if (this.isEmpty() && this.emptyCallback) {
+        this.emptyCallback();
+      }
+    });
   }
 
 }
